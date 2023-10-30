b4d2d94b86a6b98c946fbfdcafb4e6ec9208b7d2
hasyaayunisonia <hasya.ayuni.tif20@polban.ac.id>
2023-10-30 09:49:25 +0700

    mvc net core app

---
+++ ASPNETRestaurantWeb/ASPNETRestaurantWeb/library.ts
@@ -0,0 +1,12 @@
+﻿import * as $ from 'jquery';
+
+var jqtest = {
+    showMsg: function (): void {
+        let v: any = jQuery.fn.jquery.toString();
+        let content: any = $("#ts-example-2")[0].innerHTML;
+        alert(content.toString() + " " + v + "!!");
+        $("#ts-example-2")[0].innerHTML = content + " " + v + "!!";
+    }
+};
+
+jqtest.showMsg();
